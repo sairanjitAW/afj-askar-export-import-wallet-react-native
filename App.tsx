0118dcd08dd15b5cc88196c5bbd4e01bc93304da
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-05-03 15:20:33 +0530

    feat: use zip file

--- App.tsx
+++ App.tsx
@@ -22,6 +22,7 @@ import React from 'react';
 import {Button, SafeAreaView, View} from 'react-native';
 import {pickSingle, types} from 'react-native-document-picker';
 import RNFS from 'react-native-fs';
+import {zip, unzip} from 'react-native-zip-archive';
 
 const getSqliteAgentOptions = (
   name: string,
@@ -126,49 +127,64 @@ const App = () => {
   };
 
   const exportWallet = async () => {
-    const documentDirectory = RNFS.DownloadDirectoryPath;
-    const backupDirectory = `${documentDirectory}/Wallet_Backup`;
+    try {
+      const documentDirectory = RNFS.DownloadDirectoryPath;
+      const date = new Date();
+      const dformat = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+      const backupDirectory = `${documentDirectory}/Wallet_Backup_${dformat}`;
 
-    const destFileExists = await RNFS.exists(backupDirectory);
-    if (destFileExists) {
-      await RNFS.unlink(backupDirectory);
+      const destFileExists = await RNFS.exists(backupDirectory);
+      if (destFileExists) {
+        await RNFS.unlink(backupDirectory);
+      }
+
+      const WALLET_FILE_NAME = 'SSI_Wallet';
+      const WALLET_FILE_NAME_DATE = `SSI_Wallet_${dformat}`;
+
+      await RNFS.mkdir(backupDirectory);
+      const encryptedFileName = `${WALLET_FILE_NAME}.wallet`;
+      const encryptedFileLocation = `${backupDirectory}/${encryptedFileName}`;
+      const destinationZipPath = `${documentDirectory}/${WALLET_FILE_NAME_DATE}.zip`;
+
+      await bobAgent.initialize();
+      const bobBasicMessageRepository = bobAgent.dependencyManager.resolve(
+        BasicMessageRepository,
+      );
+
+      const basicMessageRecord = new BasicMessageRecord({
+        id: 'some-id',
+        connectionId: 'connId',
+        content: 'hello',
+        role: BasicMessageRole.Receiver,
+        sentTime: 'sentIt',
+      });
+
+      // Save in wallet
+      await bobBasicMessageRepository.save(
+        bobAgent.context,
+        basicMessageRecord,
+      );
+
+      if (!bobAgent.config.walletConfig) {
+        throw new Error('No wallet config on bobAgent');
+      }
+
+      const backupKey = 'SomeRandomBackUpKey';
+
+      console.log('bobAgent.wallet.isProvisioned', encryptedFileLocation);
+
+      // Create backup and delete wallet
+      await bobAgent.wallet.export({
+        path: encryptedFileLocation,
+        key: backupKey,
+      });
+      await bobAgent.wallet.delete();
+
+      const res = await zip(backupDirectory, destinationZipPath);
+      console.log('res', res);
+    } catch (error) {
+      console.error('Error in exporting wallet', error);
     }
-
-    const date = new Date();
-    const dformat = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
-    const WALLET_FILE_NAME = `SSI_Wallet_${dformat}`;
-
-    await RNFS.mkdir(backupDirectory);
-    const encryptedFileName = `${WALLET_FILE_NAME}.wallet`;
-    const encryptedFileLocation = `${backupDirectory}/${encryptedFileName}`;
-
-    await bobAgent.initialize();
-    const bobBasicMessageRepository = bobAgent.dependencyManager.resolve(
-      BasicMessageRepository,
-    );
-
-    const basicMessageRecord = new BasicMessageRecord({
-      id: 'some-id',
-      connectionId: 'connId',
-      content: 'hello',
-      role: BasicMessageRole.Receiver,
-      sentTime: 'sentIt',
-    });
-
-    // Save in wallet
-    await bobBasicMessageRepository.save(bobAgent.context, basicMessageRecord);
-
-    if (!bobAgent.config.walletConfig) {
-      throw new Error('No wallet config on bobAgent');
-    }
-
-    const backupKey = 'SomeRandomBackUpKey';
-
-    console.log('bobAgent.wallet.isProvisioned', encryptedFileLocation);
-
-    // Create backup and delete wallet
-    await bobAgent.wallet.export({path: encryptedFileLocation, key: backupKey});
-    await bobAgent.wallet.delete();
   };
 
   const importWallet = async () => {
@@ -186,7 +202,7 @@ const App = () => {
       });
 
       const pickedFile = await pickSingle({
-        type: [types.allFiles],
+        type: [types.zip],
         copyTo: 'documentDirectory',
       });
 
@@ -195,7 +211,21 @@ const App = () => {
         return;
       }
 
+      const restoreDirectoryPath = RNFS.DocumentDirectoryPath;
+
+      const WALLET_FILE_NAME = 'SSI_Wallet';
+
+      const walletFilePath = `${restoreDirectoryPath}/${WALLET_FILE_NAME}.wallet`;
+
       const selectedFilePath = await RNFS.stat(pickedFile.fileCopyUri);
+      console.log(
+        'selectedFilePath',
+        selectedFilePath.path,
+        restoreDirectoryPath,
+      );
+
+      const val = await unzip(selectedFilePath.path, restoreDirectoryPath);
+      console.log('val', val);
 
       const backupKey = 'SomeRandomBackUpKey';
 
@@ -205,7 +235,7 @@ const App = () => {
           key: 'newWalletKey',
           keyDerivationMethod: KeyDerivationMethod.Argon2IInt,
         },
-        {path: selectedFilePath.path, key: backupKey},
+        {path: walletFilePath, key: backupKey},
       );
       await bobAgent.wallet.initialize({
         id: 'oldWalletId',
